a2a04c612914feb30bb583edfd651415cfe0c82b
Alexander Marchuk <alexander.g.marchuk@gmail.com>
2015-04-20 00:09:44 +0600

    Работа над проектом GoTripleStore

--- GoTripleStore/GoGraphStringBased.cs
+++ GoTripleStore/GoGraphStringBased.cs
@@ -11,26 +11,81 @@ namespace GoTripleStore
     public class GoGraphStringBased : IGraph<Triple<string, string, ObjectVariants>>
     {
         private TableView table;
+        private IndexViewImmutable<TripleSPO> spo_ind_arr;
+        private IndexDynamic<TripleSPO, IndexViewImmutable<TripleSPO>> spo_ind;
         public GoGraphStringBased(string path)
         {
             PType tp_tabelement = new PTypeRecord(
                 new NamedType("subject", new PType(PTypeEnumeration.sstring)),
                 new NamedType("predicate", new PType(PTypeEnumeration.sstring)),
                 new NamedType("obj", ObjectVariantsPolarType.ObjectVariantPolarType));
+            Func<object,TripleSPO> keyproducer = v =>
+                {
+                    object[] va = (object[])((object[])v)[1];
+                    return new TripleSPO()
+                    {
+                        triple = new Triple<string, string, ObjectVariants>((string)va[0], (string)va[1],
+                            ObjectVariants.CreateLiteralNode(false))
+                    };
+                };
+            // Опорная таблица
             table = new TableView(path + "stable", tp_tabelement);
+            // Индекс spo
+            spo_ind_arr = new IndexViewImmutable<TripleSPO>(path + "spo_ind")
+            {
+                Table = table,
+                KeyProducer = keyproducer 
+            };
+            spo_ind = new IndexDynamic<TripleSPO, IndexViewImmutable<TripleSPO>>(false)
+            {
+                Table = table,
+                IndexArray = spo_ind_arr,
+                KeyProducer = keyproducer
+            };
         }
 
         public void Build(IEnumerable<Triple<string, string, ObjectVariants>> triples)
         {
-            //var query = triples.Select(tr => new object[] { tr.Subject, tr.Predicate, tr.Object.ToWritable() })
-            //    .Take(10).ToArray();
-            //table.Fill(query);
             table.Fill(triples.Select(tr => new object[] { tr.Subject, tr.Predicate, tr.Object.ToWritable() }));
+            spo_ind_arr.Build();
         }
 
         public IEnumerable<Triple<string, string, ObjectVariants>> Search(object subject = null, object predicate = null, ObjectVariants obj = null)
         {
-            throw new NotImplementedException();
+            if (subject != null)
+            {
+                string ssubj = (string)subject;
+                string spred = (string)predicate;
+                TripleSPO key_triple = new TripleSPO() { triple = new Triple<string,string,ObjectVariants>(ssubj, spred, null) };
+                PaEntry tab_entity = table.TableCell.Root.Element(0);
+                IEnumerable<PaEntry> entities = spo_ind.GetAllByKey(key_triple);
+                var ou_triples = entities.Select(ent =>
+                {
+                    object[] three = (object[])(((object[])ent.Get())[1]);
+                    return new Triple<string, string, ObjectVariants>((string)three[0], (string)three[1], null);
+                });
+                return ou_triples;
+            }
+            else throw new NotImplementedException();
         }
+
+        // Структуры, играющие роль ключа
+        public class TripleSPO : IComparable
+        {
+            public Triple<string, string, ObjectVariants> triple { get; set; }
+            public int CompareTo(object another)
+            {
+                if (!(another is TripleSPO)) throw new Exception("kdjfk");
+                TripleSPO ano = (TripleSPO)another;
+                int cmp = triple.Subject.CompareTo(ano.triple.Subject);
+                if (cmp == 0 && ano.triple.Predicate != null)
+                {
+                    cmp = triple.Predicate.CompareTo(ano.triple.Predicate);
+                    //if (cmp == 0) cmp = triple.Object.CompareTo(ano.triple.Object);
+                }
+                return cmp;
+            }
+        }
+
     }
 }
--- GoTripleStore/Program.cs
+++ GoTripleStore/Program.cs
@@ -14,7 +14,14 @@ namespace GoTripleStore
             var query = ReadTripleStringsFromTurtle.LoadGraph(Config.Source_data_folder_path + "1.ttl");
             //Console.WriteLine(query.Count());
             GoGraphStringBased ggraph = new GoGraphStringBased(path);
-            ggraph.Build(query);
+            //ggraph.Build(query);
+            var search_query = ggraph.Search("http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/dataFromProducer10/Product468",
+                "http://www.w3.org/2000/01/rdf-schema#label", null);
+            foreach (var t in search_query)
+            {
+                Console.WriteLine("Triple: {0} {1}", t.Subject, t.Predicate);
+            }
+            
         }
     }
 }
