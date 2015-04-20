5101814335bb9a7c2c684cbd9ecec3c328e56565
sergey303 <s@s.s>
2015-04-20 09:49:02 +0600

    comparer

--- RDFTripleStore/Compare2-3.cs
+++ RDFTripleStore/Compare2-3.cs
@@ -45,7 +45,8 @@ namespace RDFTripleStore
             throw new ArgumentException();
         }
     }
-    class Comparer3 : Comparer2, IComparer<Comparer3>, IComparable<Comparer3>, IComparable
+
+    public class Comparer3 : Comparer2, IComparer<Comparer3>, IComparable<Comparer3>, IComparable, IComparer<IComparable>
     {
 
         private readonly IComparable k3;
@@ -56,10 +57,7 @@ namespace RDFTripleStore
             this.k3 = k3;
         }
 
-        public int Compare(Comparer3 x, Comparer3 y)
-        {
-            return x.CompareTo(y);
-        }
+  
 
         public int CompareTo(Comparer3 other)
         {
@@ -79,15 +77,26 @@ namespace RDFTripleStore
 
         public new int CompareTo(object obj)
         {
-            if (obj is Comparer3) return this.CompareTo((Comparer3)obj);
-            if (obj is Comparer2) return ((Comparer2)this).CompareTo((Comparer2)obj);
+            if (obj is Comparer3) return this.CompareTo((Comparer3) obj);
+            if (obj is Comparer2) return ((Comparer2) this).CompareTo((Comparer2) obj);
             if (obj is IComparable)
             {
-                int compareK1 = k1.CompareTo(((IComparable)obj));
+                int compareK1 = k1.CompareTo(((IComparable) obj));
                 return compareK1 == 0 ? 0 : compareK1 > 0 ? 1 : -1;
             }
             throw new ArgumentException();
         }
-
+    //________________________________Comparer____________________________________________________
+        public static Comparer3 Comparer = new Comparer3(null, null, null);
+        public int Compare(Comparer3 x, Comparer3 y)
+        {
+            return x.CompareTo(y);
+        }
+        public int Compare(IComparable x, IComparable y)
+        {
+            if (x is Comparer3) return ((Comparer3) x).CompareTo(y);
+            if (x is Comparer2) return ((Comparer2)x).CompareTo(y);
+            return x.CompareTo(y);
+        }
     }
 }
