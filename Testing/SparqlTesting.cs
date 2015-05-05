62e725d345a832d8519d042c4562db3206c6aef8
sergey303 <s@s.s>
2015-05-05 16:25:31 +0600

    computing

--- Testing/SparqlTesting.cs
+++ Testing/SparqlTesting.cs
@@ -47,13 +47,13 @@ WHERE {
             SparqlStore2 sparqlStore = new SparqlStore2("../../../Databases/");
             if (load)
                 sparqlStore.ReloadFrom(Config.Source_data_folder_path + millions + ".ttl");
-            RunBerlinsWithConstants(sparqlStore, millions);
-            //   RunBerlinsParameters(sparqlStore, millions);
+            //   RunBerlinsWithConstants(sparqlStore, millions);
+            RunBerlinsParameters(sparqlStore, millions);
         }
 
         public static void RunBerlinsParameters(SparqlStore2 ts, int millions)
         {
-
+           ts.Warmup();
             Console.WriteLine("bsbm parametered");
             var paramvaluesFilePath = string.Format(@"..\..\examples\bsbm\queries\parameters\param values for{0} m.txt", millions);
             //            using (StreamWriter streamQueryParameters = new StreamWriter(paramvaluesFilePath))
