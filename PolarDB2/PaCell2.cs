51dfd3880fbba3aa592be8bf2b97961501e5409b
Alexander Marchuk <alexander.g.marchuk@gmail.com>
2015-07-06 13:22:44 +0600

    Работа над проектом PolarDB2

--- PolarDB2/PaCell2.cs
+++ PolarDB2/PaCell2.cs
@@ -54,5 +54,66 @@ namespace PolarDB2
             int v = this.br.ReadByte();
             return v;
         }
+        /// <summary>
+        /// Читает P-объект из бинарного ридера, начиная с текущего места
+        /// </summary>
+        /// <param name="typ"></param>
+        /// <param name="br"></param>
+        /// <returns></returns>
+        private static object GetPObject(PType typ, System.IO.BinaryReader br)
+        {
+            switch (typ.Vid)
+            {
+                case PTypeEnumeration.none: return null;
+                case PTypeEnumeration.boolean: return br.ReadBoolean();
+                case PTypeEnumeration.integer: return br.ReadInt32();
+                case PTypeEnumeration.longinteger: return br.ReadInt64();
+                case PTypeEnumeration.real: return br.ReadDouble();
+                case PTypeEnumeration.@byte: return br.ReadByte();
+                case PTypeEnumeration.fstring:
+                    {
+                        //int len = ((PTypeFString)typ).Length;
+                        int size = ((PTypeFString)typ).Size;
+                        byte[] arr = new byte[size];
+                        arr = br.ReadBytes(size);
+                        string s = System.Text.Encoding.Unicode.GetString(arr);
+                        return s;
+                    }
+                case PTypeEnumeration.sstring:
+                    {
+                        int len = br.ReadInt32();
+                        char[] chrs = br.ReadChars(len);
+                        return new string(chrs);
+                    }
+                case PTypeEnumeration.record:
+                    {
+                        PTypeRecord r_tp = (PTypeRecord)typ;
+                        object[] fields = new object[r_tp.Fields.Length];
+                        for (int i = 0; i < r_tp.Fields.Length; i++)
+                        {
+                            fields[i] = GetPObject(r_tp.Fields[i].Type, br);
+                        }
+                        return fields;
+                    }
+                case PTypeEnumeration.sequence:
+                    {
+                        PTypeSequence mts = (PTypeSequence)typ;
+                        PType tel = mts.ElementType;
+                        long llen = br.ReadInt64();
+                        object[] els = new object[llen];
+                        for (long ii = 0; ii < llen; ii++) els[ii] = GetPObject(tel, br);
+                        return els;
+                    }
+                case PTypeEnumeration.union:
+                    {
+                        PTypeUnion mtu = (PTypeUnion)typ;
+                        int v = br.ReadByte();
+                        PType mt = mtu.Variants[v].Type;
+                        return new object[] { v, GetPObject(mt, br) };
+                    }
+
+                default: throw new Exception("Err in TPath Get(): type is not implemented " + typ.Vid);
+            }
+        }
     }
 }
--- PolarDB2/PaEntry2.cs
+++ PolarDB2/PaEntry2.cs
@@ -13,7 +13,7 @@ namespace PolarDB2
         private PaCell2 cell;
         
         // Пустое значение. Часто используется в качестве указания, что вход не найден: 
-        // var found = entry.Where(en => predicate).DefaultIfEmpty(PaEntry.Empty).First(); if (found.IsEmpty()) ...
+        // var found = entry.Where(en => predicate).DefaultIfEmpty(PaEntry2.Empty).First(); if (found.IsEmpty()) ...
         private static PaEntry2 _empty = new PaEntry2() { cell = null, offset = Int64.MinValue, tp = null };
         public static PaEntry2 Empty { get { return _empty; } }
         public bool IsEmpty { get { return offset == Int64.MinValue; } }
@@ -24,7 +24,219 @@ namespace PolarDB2
             this.offset = offset;
             this.cell = cell;
         }
-        // Пропускает поле, выдает адрес, следующий за ним. Указатель никуда не установлен 
+
+        // ===============================================
+        // ============== Методы доступа =================
+        // ===============================================
+
+        // ========== Для записей =========
+        public PaEntry2 Field(int index)
+        {
+            if (tp.Vid != PTypeEnumeration.record) throw new Exception("Err in TPath formula: Field can't be applied to structure of vid " + tp.Vid);
+            PTypeRecord mtr = (PTypeRecord)tp;
+            if (index >= mtr.Fields.Length) throw new Exception("Err in TPath formula: index of Field is too large " + index);
+
+            long pos = this.offset;
+            for (int i = 0; i < index; i++)
+            {
+                PType t = mtr.Fields[i].Type;
+                if (t.HasNoTail) pos += t.HeadSize;
+                else
+                {
+                    pos = Skip(t, pos);
+                }
+            }
+            return new PaEntry2(mtr.Fields[index].Type, pos, cell);
+        }
+        // ========== Для последовательностей =========
+        // Подсчитывает число элементов последовательности
+        public long Count()
+        {
+            if (tp.Vid != PTypeEnumeration.sequence) throw new Exception("Err in TPath formula: Count() can't be applyed to structure of vid " + tp.Vid);
+            // Для внешних последовательностей длину берем из объекта cell
+            if (this.offset == this.cell.Root.offset) return this.cell.nElements;
+
+            return cell.ReadLong(this.offset);
+        }
+        public PaEntry2 Element(long index)
+        {
+            if (tp.Vid != PTypeEnumeration.sequence) throw new Exception("Err in TPath formula: Element() can't be applyed to structure of vid " + tp.Vid);
+            PTypeSequence mts = (PTypeSequence)tp;
+            PType t = mts.ElementType;
+            long llen = this.Count();
+            if (index < 0 || index >= llen) throw new Exception("Err in TPath formula: wrong index of Element " + index);
+            // для внешних равноэлементных последовательностей - специальная формула
+            if (t.HasNoTail || index == 0) return new PaEntry2(t, this.offset + 8 + index * t.HeadSize, cell);
+            //cell.SetOffset(this.offset); //Убрал
+            long pos = this.offset + 8;
+            for (long ii = 0; ii < index; ii++)
+            {
+                pos = Skip(t, pos);
+            }
+            return new PaEntry2(t, pos, cell);
+        }
+        // TODO: Рассмотреть целесообразность этого метода
+        private PaEntry2 ElementUnchecked(long index)
+        {
+            PType t = ((PTypeSequence)tp).ElementType;
+            if (t.HasNoTail) return new PaEntry2(t, this.offset + 8 + index * t.HeadSize, cell);
+            long pos = this.offset + 8;
+            for (long ii = 0; ii < index; ii++)
+            {
+                pos = Skip(t, pos);
+            }
+            return new PaEntry2(t, pos, cell);
+        }
+        public IEnumerable<PaEntry2> Elements()
+        {
+            if (tp.Vid != PTypeEnumeration.sequence) throw new Exception("Err in TPath formula: Elements() can't be applyed to structure of vid " + tp.Vid);
+            PTypeSequence mts = (PTypeSequence)tp;
+            PType t = mts.ElementType;
+            if (t.Vid == PTypeEnumeration.character) throw new Exception("Method Elements() can't be applied to strings");
+            //cell.SetOffset(this.offset);
+            //long llen = cell.br.ReadInt64();
+            long llen = this.Count();
+            if (llen > 0)
+            {
+                long offset = cell.GetOffset();
+                PaEntry2 entry = new PaEntry2(t, offset, cell);
+                for (long ii = 0; ii < llen; ii++)
+                {
+                    entry.offset = offset;
+                    if (ii < llen - 1) offset = entry.Skip(t, offset);
+                    yield return entry;
+                }
+            }
+        }
+        public IEnumerable<PaEntry2> Elements(long start, long number)
+        {
+            if (number > 0)
+            {
+                if (tp.Vid != PTypeEnumeration.sequence) throw new Exception("Err in TPath formula: Elements() can't be applyed to structure of vid " + tp.Vid);
+                PTypeSequence mts = (PTypeSequence)tp;
+                PType t = mts.ElementType;
+                PaEntry2 element = this.Element(start);
+                if (t.HasNoTail)
+                {
+                    int size = t.HeadSize;
+                    for (long ii = 0; ii < number; ii++)
+                    {
+                        yield return element;
+                        element.offset += size;
+                    }
+                }
+                else
+                {
+                    long offset = element.offset;
+                    for (long ii = 0; ii < number; ii++)
+                    {
+                        element.offset = offset;
+                        if (ii < number - 1) offset = element.Skip(t, offset);
+                        yield return element;
+                    }
+                }
+            }
+        }
+        // ========== Для объединений =========
+        public int Tag()
+        {
+            if (tp.Vid != PTypeEnumeration.union) throw new Exception("Err: Tag() needs union");
+            cell.SetOffset(offset);
+            return cell.br.ReadByte();
+        }
+        public PaEntry2 UElement()
+        {
+            int tag = this.Tag();
+            PTypeUnion ptu = ((PTypeUnion)this.tp);
+            if (tag < 0 || tag >= ptu.variants.Length) throw new Exception("Err: tag is out of bound");
+            PType tel = ptu.variants[tag].Type;
+            return new PaEntry2(tel, offset + 1, cell);
+        }
+        public PaEntry2 UElementUnchecked(int tag)
+        {
+            PTypeUnion ptu = ((PTypeUnion)this.tp);
+            if (tag < 0 || tag >= ptu.variants.Length) throw new Exception("Err: tag is out of bound");
+            PType tel = ptu.variants[tag].Type;
+            return new PaEntry2(tel, offset + 1, cell);
+        }
+
+        public PValue GetValue()
+        {
+            this.cell.SetOffset(offset);
+            return new PValue(this.tp, this.offset, GetPObject(tp, cell.br));
+        }
+        public object Get()
+        {
+            this.cell.SetOffset(offset);
+            return GetPObject(tp, cell.br);
+        }
+        // Следующие два метода не стоит применять в режимах отложенных вычислений. Есть побочный эффект
+        public IEnumerable<object> ElementValues()
+        {
+            if (tp.Vid != PTypeEnumeration.sequence) throw new Exception("Err in TPath formula: ElementValues() can't be applyed to structure of vid " + tp.Vid);
+            PType t = ((PTypeSequence)tp).ElementType;
+            long ll = this.Count();
+            if (ll == 0) yield break;
+            PaEntry2 first = this.Element(0);
+            this.cell.SetOffset(first.offset);
+            for (long ii = 0; ii < ll; ii++)
+            {
+                yield return GetPObject(t, this.cell.br);
+            }
+        }
+        public IEnumerable<object> ElementValues(long start, long number)
+        {
+            if (tp.Vid != PTypeEnumeration.sequence) throw new Exception("Err in TPath formula: ElementValues() can't be applyed to structure of vid " + tp.Vid);
+            PType t = ((PTypeSequence)tp).ElementType;
+            //if (!t.HasNoTail) throw new Exception("Method ElementValues() can't be applied to tail types");
+            long ll = this.Count();
+            // Надо проверить соответствие диапазона количеству элементов
+            if (start < 0 || start + number > ll) throw new Exception("Err: Diapason is out of range");
+            if (number == 0) yield break;
+            PaEntry2 first = this.Element(start);
+            this.cell.SetOffset(first.offset);
+            for (long ii = 0; ii < number; ii++)
+            {
+                yield return GetPObject(t, this.cell.br);
+            }
+        }
+        // depricated: недодуманная идея, использовать не рекомендуется
+        public void Scan(Func<object, bool> handler)
+        {
+            if (tp.Vid != PTypeEnumeration.sequence) throw new Exception("Err in TPath formula: ElementValues() can't be applyed to structure of vid " + tp.Vid);
+            PTypeSequence mts = (PTypeSequence)tp;
+            PType t = mts.ElementType;
+            long ll = this.Count();
+            if (ll == 0) return;
+            PaEntry2 first = this.Element(0);
+            this.cell.SetOffset(first.offset);
+            for (long ii = 0; ii < ll; ii++)
+            {
+                object pvalue = GetPObject(t, this.cell.br);
+                bool ok = handler(pvalue);
+                if (!ok) throw new Exception("Scan handler catched 'false' at element " + ii);
+            }
+        }
+        // Возможно, уже лучше: передавать нужно и offset
+        public void Scan(Func<long, object, bool> handler)
+        {
+            if (tp.Vid != PTypeEnumeration.sequence) throw new Exception("Err in TPath formula: ElementValues() can't be applyed to structure of vid " + tp.Vid);
+            PTypeSequence mts = (PTypeSequence)tp;
+            PType t = mts.ElementType;
+            long ll = this.Count();
+            if (ll == 0) return;
+            PaEntry2 first = this.Element(0);
+            this.cell.SetOffset(first.offset);
+            for (long ii = 0; ii < ll; ii++)
+            {
+                long off = this.cell.GetOffset(); //this.offset;
+                object pvalue = GetPObject(t, this.cell.br);
+                bool ok = handler(off, pvalue);
+                if (!ok) throw new Exception("Scan handler catched 'false' at element " + ii);
+            }
+        }
+
+        // Техническая процедура Пропускает поле, выдает адрес, следующий за ним. Указатель никуда не установлен 
         private long Skip(PType tp, long off)
         {
             if (tp.HasNoTail) return off + tp.HeadSize;
